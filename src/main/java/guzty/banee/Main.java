57b95ad0c75c47b6225f2a3c422c2c4179e26393
Banee Ishaque K <Baneeishaque@gmail.com>
2023-09-21 19:44:06 +0530

    Files Traverse

--- src/main/java/guzty/banee/Main.java
+++ src/main/java/guzty/banee/Main.java
@@ -10,8 +10,11 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.ExecutionException;
 
 public class Main {
@@ -37,7 +40,24 @@ public class Main {
             }
             List<QueryDocumentSnapshot> documents = querySnapshot.getDocuments();
             for (QueryDocumentSnapshot document : documents) {
-                System.out.println("document = " + document.getData());
+//                System.out.println("document = " + document.getData());
+
+                Map<String, Object> data = document.getData();
+                for (final File fileEntry : Objects.requireNonNull(new File("~/upload_guzty_product_templates/assets").listFiles())) {
+
+                    String filePath = fileEntry.getPath().replaceFirst("~/upload_guzty_product_templates/assets/", "");
+                    if (data.get("name") == filePath.substring(0, filePath.indexOf("/"))) {
+
+                        if (filePath.contains(".json")) {
+
+                            System.out.println("Write : " + filePath);
+
+                        } else {
+
+                            System.out.println("Upload : " + filePath);
+                        }
+                    }
+                }
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
