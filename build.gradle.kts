55d1e563353318fd7a20b9a66ac5d45d02f32994
Banee Ishaque K <Baneeishaque@gmail.com>
2023-09-21 20:05:26 +0530

    Run App.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("application")
 }
 
 group = "org.example"
@@ -11,10 +12,16 @@ repositories {
 
 dependencies {
     compileOnly("com.google.firebase:firebase-admin:9.2.0")
+    implementation("com.google.auth:google-auth-library-oauth2-http:1.19.0")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("com.google.guava:guava:32.1.1-jre")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+application {
+    mainClass.set("guzty.banee.Main")
 }
