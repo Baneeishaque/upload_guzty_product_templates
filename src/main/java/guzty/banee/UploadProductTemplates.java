2ecff5625da9abfb0b94abe6ec622bc7ccf2f61c
Banee Ishaque K <Baneeishaque@gmail.com>
2023-09-28 02:14:40 +0530

    Logs

--- src/main/java/guzty/banee/UploadProductTemplates.java
+++ src/main/java/guzty/banee/UploadProductTemplates.java
@@ -1,28 +1,27 @@
 package guzty.banee;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
 import com.google.api.core.ApiFuture;
 import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.firestore.*;
-import com.google.cloud.storage.Blob;
-import com.google.cloud.storage.*;
+import com.google.cloud.firestore.Filter;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.QueryDocumentSnapshot;
+import com.google.cloud.firestore.QuerySnapshot;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
-import net.sf.jmimemagic.Magic;
-import net.sf.jmimemagic.MagicException;
-import net.sf.jmimemagic.MagicMatchNotFoundException;
-import net.sf.jmimemagic.MagicParseException;
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.FilenameUtils;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.ExecutionException;
 
 public class UploadProductTemplates {
@@ -71,89 +70,89 @@ public class UploadProductTemplates {
                                     ProductJsonModal productJsonModal = mapper.readValue(FileUtils.readFileToString(fileEntry3, StandardCharsets.UTF_8), ProductJsonModal.class);
                                     System.out.println("productJsonModal = " + productJsonModal);
 
-                                    ApiFuture<DocumentSnapshot> querySnapshotApiFuture = db.collection("settings").document("settings").get();
-                                    DocumentSnapshot settingsDocument = querySnapshotApiFuture.get();
-                                    int demoProductId = Integer.parseInt(Objects.requireNonNull(settingsDocument.get("demoProductId")).toString());
-                                    String id = "GZDP" + demoProductId;
-//                                        System.out.println("id = " + id);
-
-                                    HashMap<String, Object> hashMap = new HashMap<>();
-                                    hashMap.put("demoProductId", String.valueOf(++demoProductId));
-
-                                    settingsDocument.getReference().update(hashMap);
-
-                                    DocumentReference demoProductReference = db.collection("demoProducts").document(id);
-
-                                    ProductModel productModal = new ProductModel(LocalDateTime.now(), false, true, productJsonModal.getProductName().trim(), images, productJsonModal.getShortDescription(), productJsonModal.getLongDescription(), productJsonModal.getPrice(), productJsonModal.getLeadTime(), productJsonModal.getSkuSet(), 0, "", "", "", true, true, data.get("id").toString(), categoryName, 0, 0, 0, 0, 0, productJsonModal.getProductType().equals("Veg"), new HashMap<>(), true, productJsonModal.getOrderType(), productJsonModal.getOrderType(), productJsonModal.getMaxCount(), productJsonModal.getMinCount(), productJsonModal.getGst(), new ArrayList<>() {
-                                    }, new ArrayList<>(), 0, 0, productJsonModal.getLocalDelicacies(), false, productJsonModal.getVarients(), demoProductReference.getId(), demoProductReference, getProductTypeId(productJsonModal.getProductType(), filePath));
-
-//                                                search: setSearchParam(productJson.productName.trim()),
-
-//                                        System.out.println("productModal = " + productModal);
-
-                                    ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
-//                                        System.out.println("productModal JSON = " + ow.writeValueAsString(productModal));
-
-                                    Map<String, Object> demoProductDocument = new HashMap<>();
-                                    demoProductDocument.put("createdTime", FieldValue.serverTimestamp());
-                                    demoProductDocument.put("deleted", productModal.deleted);
-                                    demoProductDocument.put("available", productModal.available);
-                                    demoProductDocument.put("name", productModal.name);
-
-                                    demoProductDocument.put("imageUrls", new ArrayList<>(productModal.imageUrls));
-
-                                    demoProductDocument.put("shortDescription", productModal.shortDescription);
-                                    demoProductDocument.put("longDescription", productModal.longDescription);
-                                    demoProductDocument.put("price", 0);
-                                    demoProductDocument.put("leadingTime", 0);
-                                    demoProductDocument.put("skuSet", "");
-                                    demoProductDocument.put("specialOffer", productModal.specialOffer);
-                                    demoProductDocument.put("productId", productModal.productId);
-                                    demoProductDocument.put("vendorId", productModal.vendorId);
-                                    demoProductDocument.put("mbuVerified", productModal.mbuVerified);
-                                    demoProductDocument.put("mbuAvailable", productModal.mbuAvailable);
-                                    demoProductDocument.put("categoryId", productModal.categoryId);
-                                    demoProductDocument.put("categoryName", productModal.categoryName);
-
-                                    demoProductDocument.put("oneRating", productModal.oneRating);
-                                    demoProductDocument.put("twoRating", productModal.twoRating);
-                                    demoProductDocument.put("threeRating", productModal.threeRating);
-                                    demoProductDocument.put("fourRating", productModal.fourRating);
-                                    demoProductDocument.put("fiveRating", productModal.fiveRating);
-                                    demoProductDocument.put("veg", productModal.veg);
-
-                                    demoProductDocument.put("position", productModal.position);
-
-                                    demoProductDocument.put("verified", productModal.verified);
-
-                                    demoProductDocument.put("ordersType", new ArrayList<>(productModal.ordersType));
-                                    demoProductDocument.put("selectedOrdersType", new ArrayList<>(productModal.selectedOrdersType));
-
-                                    demoProductDocument.put("maxOrder", productModal.maxOrder);
-                                    demoProductDocument.put("minOrder", productModal.minOrder);
-                                    demoProductDocument.put("gst", productModal.gst);
-
-                                    demoProductDocument.put("search", new ArrayList<>(productModal.search));
-
-                                    demoProductDocument.put("keywords", new ArrayList<>(productModal.keywords));
-
-                                    demoProductDocument.put("lat", productModal.lat);
-                                    demoProductDocument.put("long", productModal.longitude);
-                                    demoProductDocument.put("localDelicacies", productModal.localDelicacies);
-                                    demoProductDocument.put("instaKitchen", productModal.instaKitchen);
-
-                                    demoProductDocument.put("variants", List.of());
-
-                                    demoProductDocument.put("demoProductId", productModal.demoProductId);
-                                    demoProductDocument.put("reference", productModal.reference);
-                                    demoProductDocument.put("productTypeId", productModal.productTypeId);
-
-                                    ApiFuture<WriteResult> writeResultApiFuture = db.collection("demoProducts").document(id).set(demoProductDocument);
-                                    System.out.println("Update time : " + writeResultApiFuture.get().getUpdateTime());
-
-//                                    System.exit(0);
-
-                                    images.clear();
+//                                    ApiFuture<DocumentSnapshot> querySnapshotApiFuture = db.collection("settings").document("settings").get();
+//                                    DocumentSnapshot settingsDocument = querySnapshotApiFuture.get();
+//                                    int demoProductId = Integer.parseInt(Objects.requireNonNull(settingsDocument.get("demoProductId")).toString());
+//                                    String id = "GZDP" + demoProductId;
+////                                        System.out.println("id = " + id);
+//
+//                                    HashMap<String, Object> hashMap = new HashMap<>();
+//                                    hashMap.put("demoProductId", String.valueOf(++demoProductId));
+//
+//                                    settingsDocument.getReference().update(hashMap);
+//
+//                                    DocumentReference demoProductReference = db.collection("demoProducts").document(id);
+//
+//                                    ProductModel productModal = new ProductModel(LocalDateTime.now(), false, true, productJsonModal.getProductName().trim(), images, productJsonModal.getShortDescription(), productJsonModal.getLongDescription(), productJsonModal.getPrice(), productJsonModal.getLeadTime(), productJsonModal.getSkuSet(), 0, "", "", "", true, true, data.get("id").toString(), categoryName, 0, 0, 0, 0, 0, productJsonModal.getProductType().equals("Veg"), new HashMap<>(), true, productJsonModal.getOrderType(), productJsonModal.getOrderType(), productJsonModal.getMaxCount(), productJsonModal.getMinCount(), productJsonModal.getGst(), new ArrayList<>() {
+//                                    }, new ArrayList<>(), 0, 0, productJsonModal.getLocalDelicacies(), false, productJsonModal.getVarients(), demoProductReference.getId(), demoProductReference, getProductTypeId(productJsonModal.getProductType(), filePath));
+//
+////                                                search: setSearchParam(productJson.productName.trim()),
+//
+////                                        System.out.println("productModal = " + productModal);
+//
+//                                    ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+////                                        System.out.println("productModal JSON = " + ow.writeValueAsString(productModal));
+//
+//                                    Map<String, Object> demoProductDocument = new HashMap<>();
+//                                    demoProductDocument.put("createdTime", FieldValue.serverTimestamp());
+//                                    demoProductDocument.put("deleted", productModal.deleted);
+//                                    demoProductDocument.put("available", productModal.available);
+//                                    demoProductDocument.put("name", productModal.name);
+//
+//                                    demoProductDocument.put("imageUrls", new ArrayList<>(productModal.imageUrls));
+//
+//                                    demoProductDocument.put("shortDescription", productModal.shortDescription);
+//                                    demoProductDocument.put("longDescription", productModal.longDescription);
+//                                    demoProductDocument.put("price", 0);
+//                                    demoProductDocument.put("leadingTime", 0);
+//                                    demoProductDocument.put("skuSet", "");
+//                                    demoProductDocument.put("specialOffer", productModal.specialOffer);
+//                                    demoProductDocument.put("productId", productModal.productId);
+//                                    demoProductDocument.put("vendorId", productModal.vendorId);
+//                                    demoProductDocument.put("mbuVerified", productModal.mbuVerified);
+//                                    demoProductDocument.put("mbuAvailable", productModal.mbuAvailable);
+//                                    demoProductDocument.put("categoryId", productModal.categoryId);
+//                                    demoProductDocument.put("categoryName", productModal.categoryName);
+//
+//                                    demoProductDocument.put("oneRating", productModal.oneRating);
+//                                    demoProductDocument.put("twoRating", productModal.twoRating);
+//                                    demoProductDocument.put("threeRating", productModal.threeRating);
+//                                    demoProductDocument.put("fourRating", productModal.fourRating);
+//                                    demoProductDocument.put("fiveRating", productModal.fiveRating);
+//                                    demoProductDocument.put("veg", productModal.veg);
+//
+//                                    demoProductDocument.put("position", productModal.position);
+//
+//                                    demoProductDocument.put("verified", productModal.verified);
+//
+//                                    demoProductDocument.put("ordersType", new ArrayList<>(productModal.ordersType));
+//                                    demoProductDocument.put("selectedOrdersType", new ArrayList<>(productModal.selectedOrdersType));
+//
+//                                    demoProductDocument.put("maxOrder", productModal.maxOrder);
+//                                    demoProductDocument.put("minOrder", productModal.minOrder);
+//                                    demoProductDocument.put("gst", productModal.gst);
+//
+//                                    demoProductDocument.put("search", new ArrayList<>(productModal.search));
+//
+//                                    demoProductDocument.put("keywords", new ArrayList<>(productModal.keywords));
+//
+//                                    demoProductDocument.put("lat", productModal.lat);
+//                                    demoProductDocument.put("long", productModal.longitude);
+//                                    demoProductDocument.put("localDelicacies", productModal.localDelicacies);
+//                                    demoProductDocument.put("instaKitchen", productModal.instaKitchen);
+//
+//                                    demoProductDocument.put("variants", List.of());
+//
+//                                    demoProductDocument.put("demoProductId", productModal.demoProductId);
+//                                    demoProductDocument.put("reference", productModal.reference);
+//                                    demoProductDocument.put("productTypeId", productModal.productTypeId);
+//
+//                                    ApiFuture<WriteResult> writeResultApiFuture = db.collection("demoProducts").document(id).set(demoProductDocument);
+//                                    System.out.println("Update time : " + writeResultApiFuture.get().getUpdateTime());
+//
+////                                    System.exit(0);
+//
+//                                    images.clear();
 
                                 } else {
 
@@ -162,27 +161,27 @@ public class UploadProductTemplates {
                                         String fileName = fileEntry4.getPath().replaceFirst("/home/guzty_tech/upload_guzty_product_templates/assets/", "");
                                         System.out.println("Upload : " + fileName);
 
-                                        LocalDateTime localDateTime = LocalDateTime.now();
-                                        String blobString = dateTimeFormatterDate.format(localDateTime) + "/" + dateTimeFormatterTime.format(localDateTime) + "." + FilenameUtils.getExtension(fileEntry4.getPath());
-
-                                        BlobId blobId = BlobId.of("guzty-c2dc5.appspot.com", blobString);
-                                        try {
-                                            BlobInfo blobInfo = BlobInfo.newBuilder(blobId)
-                                                    .setMetadata(Map.ofEntries(Map.entry("picked-file-path", fileName)))
-                                                    .setContentType(Magic.getMagicMatch(fileEntry4, false).getMimeType())
-                                                    .build();
-                                            Blob blob = storage.createFrom(blobInfo, fileEntry4.toPath(), Storage.BlobWriteOption.predefinedAcl(Storage.PredefinedAcl.PUBLIC_READ));
-//                                            System.out.println("image = " + blob.getMediaLink());
-                                            images.add(blob.getMediaLink());
-
-//                                            try (InputStream image = new FileInputStream(fileEntry4.getPath())) {
+//                                        LocalDateTime localDateTime = LocalDateTime.now();
+//                                        String blobString = dateTimeFormatterDate.format(localDateTime) + "/" + dateTimeFormatterTime.format(localDateTime) + "." + FilenameUtils.getExtension(fileEntry4.getPath());
 //
-//                                                Blob blob = storageClient.bucket("guzty-c2dc5.appspot.com").create(blobString, image, Magic.getMagicMatch(fileEntry4, false).getMimeType(), Bucket.BlobWriteOption.doesNotExist());
-//                                            }
-
-                                        } catch (MagicParseException | MagicMatchNotFoundException | MagicException e) {
-                                            throw new RuntimeException(e);
-                                        }
+//                                        BlobId blobId = BlobId.of("guzty-c2dc5.appspot.com", blobString);
+//                                        try {
+//                                            BlobInfo blobInfo = BlobInfo.newBuilder(blobId)
+//                                                    .setMetadata(Map.ofEntries(Map.entry("picked-file-path", fileName)))
+//                                                    .setContentType(Magic.getMagicMatch(fileEntry4, false).getMimeType())
+//                                                    .build();
+//                                            Blob blob = storage.createFrom(blobInfo, fileEntry4.toPath(), Storage.BlobWriteOption.predefinedAcl(Storage.PredefinedAcl.PUBLIC_READ));
+////                                            System.out.println("image = " + blob.getMediaLink());
+//                                            images.add(blob.getMediaLink());
+//
+////                                            try (InputStream image = new FileInputStream(fileEntry4.getPath())) {
+////
+////                                                Blob blob = storageClient.bucket("guzty-c2dc5.appspot.com").create(blobString, image, Magic.getMagicMatch(fileEntry4, false).getMimeType(), Bucket.BlobWriteOption.doesNotExist());
+////                                            }
+//
+//                                        } catch (MagicParseException | MagicMatchNotFoundException | MagicException e) {
+//                                            throw new RuntimeException(e);
+//                                        }
 //                                        System.exit(0);
                                     }
                                 }
