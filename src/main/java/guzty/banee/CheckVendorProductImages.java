64b6308c723562b0728347a116c37038896528be
Banee Ishaque K <baneeishaque@gmail.com>
2023-11-21 20:39:08 +0530

    Import Fix
    Signed-off-by: Banee Ishaque K <baneeishaque@gmail.com>

--- src/main/java/guzty/banee/CheckVendorProductImages.java
+++ src/main/java/guzty/banee/CheckVendorProductImages.java
@@ -18,6 +18,7 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
 import static guzty.banee.CheckDemoProductImages.getUrlWithoutParameters;
